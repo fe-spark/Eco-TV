7c93ac80e117d3b77806d5827da415c7c7c5a5ac
spark <spark.xiaoyu@qq.com>
2026-04-21 10:28:39 +0800

    Update interface

--- lib/store/history.dart
+++ lib/store/history.dart
@@ -7,7 +7,25 @@ class HistoryStore with ChangeNotifier, DiagnosticableTreeMixin {
   final legacyEpisodeProgressKey = 'historyEpisodeProgress';
 
   List get data {
-    return PreferenceUtil.getMap<List>(preferenceKey) ?? [];
+    final historyList = PreferenceUtil.getMap<List>(preferenceKey) ?? [];
+    return historyList
+        .map((item) => item is Map ? _normalizeHistoryItem(item) : item)
+        .toList();
+  }
+
+  Map<String, dynamic> _normalizeHistoryItem(Map item) {
+    final normalized = Map<String, dynamic>.from(item);
+    final startAt = normalized['startAt'];
+    if (startAt is num) {
+      normalized['startAt'] = max(0, startAt.toInt());
+    }
+
+    final teleplayIndex = normalized['teleplayIndex'];
+    if (teleplayIndex is num) {
+      normalized['teleplayIndex'] = max(0, teleplayIndex.toInt());
+    }
+
+    return normalized;
   }
 
   int getEpisodeStartAt({
@@ -39,10 +57,11 @@ class HistoryStore with ChangeNotifier, DiagnosticableTreeMixin {
   }
 
   void addHistory(Map<String, dynamic> history) async {
-    var newList = [history];
+    final normalizedHistory = _normalizeHistoryItem(history);
+    var newList = [normalizedHistory];
 
     for (var item in data) {
-      if (item['id'] != history['id']) {
+      if (item['id'] != normalizedHistory['id']) {
         newList.add(item);
       }
     }
